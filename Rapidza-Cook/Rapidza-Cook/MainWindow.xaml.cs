819ab6d9f50d51d1e964e2a0afa54fff55a7a7e6
Mihail Bondarenco <441044@student.fontys.nl>
2019-10-24 18:03:33 +0200

    feat(ordered products) add listbox with template for ordered products

--- Rapidza-Cook/Rapidza-Cook/MainWindow.xaml.cs
+++ Rapidza-Cook/Rapidza-Cook/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,28 @@ namespace Rapidza_Cook
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ObservableCollection<OrderedProduct> orderedProducts;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            orderedProducts = new ObservableCollection<OrderedProduct>()
+            {
+                new OrderedProduct("Margareta"),
+                new OrderedProduct("Rancho"),
+                new OrderedProduct("Rancho"),
+                new OrderedProduct("Quadra Fromagge"),
+                new OrderedProduct("Rancho"),
+                new OrderedProduct("Rancho"),
+
+            };
+
+            lbOrderedProducts.ItemsSource = orderedProducts;
+
         }
     }
 }
