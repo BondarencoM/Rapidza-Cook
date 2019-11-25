282efdaa0bcada8632de093c6529d2aa9e7a95bc
aidas <a.svegzda@student.fontys.nl>
2019-11-25 12:31:12 +0100

    Established connection

---
+++ Rapidza-Cook/Rapidza-Cook/DataReceiver.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Rapidza_Cook
+{
+    class DataReceiver
+    {
+        private TcpListener listener;
+        private bool isAborted = false;
+
+        public event EventHandler<string> messageReceived;
+        public DataReceiver(int port)
+        {
+            listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+
+            byte[] data = new byte[3000];
+            listener.AcceptTcpClientAsync().ContinueWith(handleRequest);
+        }
+
+        private void handleRequest(Task<TcpClient> task)
+        {
+            if (isAborted)
+                return;
+
+            byte[] data = new byte[3000];
+            UInt16 messageLength = 0;
+            try
+            {
+                NetworkStream stream = task.Result.GetStream();
+                stream.Read(data, 0, data.Length);
+
+                messageLength = BitConverter.ToUInt16(data, 0);
+
+                string message = Encoding.ASCII.GetString(data, 2, messageLength);
+
+                Console.WriteLine($"Received a message of {messageLength} bytes:\n{message}");
+
+                if (!string.IsNullOrWhiteSpace(message) && message != "ping")
+                    messageReceived?.Invoke(this, message);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine($"Ignoring request with invalid length prefix({messageLength}):");
+                Console.WriteLine(Encoding.ASCII.GetString(data));
+            }
+            finally
+            {
+                listener.AcceptTcpClientAsync().ContinueWith(handleRequest);
+            }
+
+        }
+
+        public void close()
+        {
+
+            isAborted = true;
+            listener.Stop();
+        }
+
+
+    }
+}
--- Rapidza-Cook/Rapidza-Cook/MainWindow.xaml.cs
+++ Rapidza-Cook/Rapidza-Cook/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -22,27 +25,73 @@ namespace Rapidza_Cook
     public partial class MainWindow : Window
     {
         private ObservableCollection<OrderedProduct> orderedProducts;
+        private DataReceiver receive = new DataReceiver(8082);
+
 
         public MainWindow()
         {
             InitializeComponent();
+            receive.messageReceived += Receive_messageReceived;
         }
+
+        public static string GetLocalIP()
+        {
+            ////look it up how to get the ip adress
+            IPHostEntry host;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            return "127.0.0.1";
+
+        }
+
+
+        private void Receive_messageReceived(object sender, string e)
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine(e);
+            var products = JsonConvert.DeserializeObject<List<OrderedProduct>>(e);
+            App.Current.Dispatcher.Invoke(
+                () =>
+                {
+                    foreach (OrderedProduct o in products)
+                    {
+                        orderedProducts.Add(o);
+                    }
+                }
+                );
+
+        }
+
+    
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             orderedProducts = new ObservableCollection<OrderedProduct>()
-            {
+            {/*
                 new OrderedProduct("Margareta"),
                 new OrderedProduct("Rancho"),
                 new OrderedProduct("Rancho"),
                 new OrderedProduct("Quadra Fromagge"),
                 new OrderedProduct("Rancho"),
                 new OrderedProduct("Rancho"),
-
-            };
-
+            */
+                };
+            MessageBox.Show(GetLocalIP());
             lbOrderedProducts.ItemsSource = orderedProducts;
-
         }
+
+        private void toRemove(object sender, RoutedEventArgs e)
+        {
+            OrderedProduct item = (sender as Button).DataContext as OrderedProduct;
+            orderedProducts.Remove(item);
+        }
+
+
     }
 }
